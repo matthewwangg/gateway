2d4d57ad2d8ace9aeea5fb428a3c1ae459902f45
Matt Wang <matthewianwang@gmail.com>
2025-08-21 20:39:31 -0400

    feat(parser): implement lexer tokenize function

--- internal/parser/lexer.go
+++ internal/parser/lexer.go
@@ -1,5 +1,12 @@
 package parser
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
 type Lexer struct {
 	Tokens   []Token
 	Position int
@@ -14,8 +21,75 @@ func NewLexer(filepath string) *Lexer {
 }
 
 func Tokenize(filepath string) []Token {
-	// Implement this
-	return []Token{}
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Printf("error opening service definition file at %s\n", filepath)
+		return nil
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Printf("error closing service definition file at %s\n", filepath)
+			return
+		}
+	}()
+
+	tokens := make([]Token, 0)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		if strings.HasPrefix(line, "- ") {
+			tokens = append(tokens, Token{
+				Type:   TOKEN_DASH,
+				Lexeme: "DASH",
+			})
+			tokens = append(tokens, Token{
+				Type:   TOKEN_VALUE,
+				Lexeme: strings.TrimPrefix(line, "- "),
+			})
+			tokens = append(tokens, Token{
+				Type:   TOKEN_NEWLINE,
+				Lexeme: "NEWLINE",
+			})
+			continue
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			tokens = append(tokens, Token{
+				Type:   TOKEN_KEY,
+				Lexeme: key,
+			})
+			tokens = append(tokens, Token{
+				Type:   TOKEN_COLON,
+				Lexeme: "COLON",
+			})
+			if len(parts[1]) > 0 {
+				tokens = append(tokens, Token{
+					Type:   TOKEN_VALUE,
+					Lexeme: value,
+				})
+			}
+			tokens = append(tokens, Token{
+				Type:   TOKEN_NEWLINE,
+				Lexeme: "NEWLINE",
+			})
+		}
+	}
+
+	tokens = append(tokens, Token{
+		Type:   TOKEN_EOF,
+		Lexeme: "EOF",
+	})
+
+	return tokens
 }
 
 func (l *Lexer) GetToken() Token {

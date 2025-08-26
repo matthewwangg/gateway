b28d9a9ceb4db8a8849843862037f05c838b3f3d
Matt Wang <77259515+matthewwangg@users.noreply.github.com>
2025-08-26 00:11:03 -0400

    feat(gateway): add JWT authentication middleware (#4)
    * feat(middleware): add JWT middleware
    
    * feat(middleware): add function to batch use middleware
    
    * feat(gateway): add basic login handler

--- internal/gateway/routes.go
+++ internal/gateway/routes.go
@@ -2,11 +2,19 @@ package gateway
 
 import (
 	"net/http"
+
+	middleware "github.com/matthewwangg/gateway/internal/middleware"
 )
 
 func (g *Gateway) SetupRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /healthz", g.HealthCheck)
-	mux.HandleFunc("GET /reload", g.Reload)
-	mux.HandleFunc("GET /services", g.Services)
-	mux.HandleFunc("POST /call", g.Call)
+	mux.HandleFunc("POST /login", g.Login)
+
+	mux.Handle("GET /reload", g.Secure(g.Reload))
+	mux.Handle("GET /services", g.Secure(g.Services))
+	mux.Handle("POST /call", g.Secure(g.Call))
+}
+
+func (g *Gateway) Secure(handler http.HandlerFunc) http.Handler {
+	return middleware.Use(handler, middleware.CheckJWTToken)
 }

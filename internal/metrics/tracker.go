7df29f212cebd2d7df02a6366529524733622488
Matt Wang <matthewianwang@gmail.com>
2025-09-02 14:18:33 -0400

    feat(metrics): add metrics tracker

---
+++ internal/metrics/tracker.go
@@ -0,0 +1,36 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type MetricsTracker struct {
+	ServiceCalls *prometheus.CounterVec
+	Requests     *prometheus.CounterVec
+}
+
+var Tracker *MetricsTracker
+
+func Init() {
+	Tracker = NewMetricsTracker()
+}
+
+func NewMetricsTracker() *MetricsTracker {
+	tracker := &MetricsTracker{
+		Requests: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "requests",
+			Help: "Number of requests to the gateway.",
+		}, []string{"endpoint", "code"}),
+		ServiceCalls: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "service_calls",
+			Help: "Number of calls to each service.",
+		}, []string{"service"}),
+	}
+
+	prometheus.MustRegister(
+		tracker.Requests,
+		tracker.ServiceCalls,
+	)
+
+	return tracker
+}
